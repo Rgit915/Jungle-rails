7815063d143020676a3bd5a26fcd96a3da5beced
Rora Alem <rora.goitom@gmail.com>
2023-11-05 04:17:37 -0500

    Update the user_params method to match the fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.new(user_params)
+
+    if @user.save
+      # User registration was successful; log in the user.
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Registration successful!'
     else
       redirect_to '/signup'
     end
@@ -14,7 +17,7 @@ class UsersController < ApplicationController
 
   private
 
-def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
-end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
