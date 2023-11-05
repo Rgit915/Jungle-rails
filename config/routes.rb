c52b5f42c67f256102dbab4126d75c62270c3cbc
Rora Alem <rora.goitom@gmail.com>
2023-11-05 01:30:10 -0400

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+  get 'users/create'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -81,12 +83,12 @@ Rails.application.routes.draw do
 
   # GifVault routes:
    # This route sends requests to our naked url to the *cool* action in the *gif* controller.
-   root to: 'gif#cool'
+   #root to: 'gif#cool'
 
    # I've created a gif controller so I have a page I can secure later.
    # This is optional (as is the root to: above).
-   get '/cool' => 'gif#cool'
-   get '/sweet' => 'gif#sweet'
+  #  get '/cool' => 'gif#cool'
+  #  get '/sweet' => 'gif#sweet'
 
    # These routes will be for signup. The first renders a form in the browse, the second will
    # receive the form and create a user in our database using the data given to us by the user.
