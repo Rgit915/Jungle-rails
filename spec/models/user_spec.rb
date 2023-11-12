5710fe4297fa22bae36f73109f6e7d7587ee60ee
Rora Alem <rora.goitom@gmail.com>
2023-11-12 07:13:05 -0500

    Add validation test for user with password & password_confirmation fields

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'should be created with password and password_confirmation fields' do
+      user = User.new(
+        email: 'test@example.com',
+        first_name: 'John',
+        last_name: 'Doe',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(user.save).to be true
+    end
+
+
+  end
 end
+
